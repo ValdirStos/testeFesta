e2451e14001822d19f96e9b520f423aef815be31
ValdirStos <valdirsantos513@gmail.com>
2023-02-08 10:15:51 -0300

    criação exercicioMatematico

---
+++ exercicioMatematico.js
@@ -0,0 +1,10 @@
+const avista= 1350;
+
+const quantidadeParcela= 12;
+
+const valorDaParcela= 138;
+
+let valorTotalPrazo = quantidadeParcela * valorDaParcela;
+console.log(valorTotalPrazo)
+let diferenca = valorTotalPrazo - avista
+console.log(diferenca)
